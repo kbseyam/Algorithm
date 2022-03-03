e5c28dc4bd4bfd441d54e1ffa69f2c7eadc43438
khalid bassam seyam <kbseyam@gmail.com>
2022-03-03 20:22:24 +0300

    Create

---
+++ Kotlin/Math/_09PrintNumbersFromNTo1.kt
@@ -0,0 +1,17 @@
+fun main() {
+    val n = readln().toInt()
+
+    for (i in n downTo 1) {
+        println(i)
+    }
+}
+
+/*
+    var n = readln().toInt()
+    println(n)
+
+    while (n != 1) {
+        n--
+        println(n)
+    }
+*/
