a8d60751250c2cd191751ab2859d6e51c8d48d1c
khalid bassam seyam <kbseyam@gmail.com>
2022-03-04 02:00:42 +0300

    Create

---
+++ Java/Math/_02HalfNumber.java
@@ -0,0 +1,10 @@
+package Java.Math;
+
+public class _02HalfNumber {
+    public static void main(String[] args) {
+        double num = 5;
+        double halfNum = num / 2;
+
+        System.out.println("Half of the " + num + " is " + halfNum);
+    }
+}
