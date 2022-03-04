2c540b34382b970f0ac5c189f810996e85b034b5
khalid bassam seyam <kbseyam@gmail.com>
2022-03-05 01:52:31 +0300

    Create

---
+++ Kotlin/Math/_10PrintSumOddNumbersfrom1ToN.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val n = readln().toInt()
+    var sum = 0
+
+    for (counter in 1..n) {
+        if (counter % 2 != 0) {
+            sum += counter
+        }
+    }
+
+    print(sum)
+}
