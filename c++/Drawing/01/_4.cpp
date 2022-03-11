9ea3a746a87082f6aeb29c7b3f14b55938301d31
khalid bassam seyam <kbseyam@gmail.com>
2022-03-11 22:47:31 +0300

    Create

---
+++ c++/Drawing/01/_4.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    short x;
+
+    do
+    {
+        cout << "X: ";
+        cin >> x;
+    } while (x < 1);
+
+    for (short i = 1; i <= x; i++) {
+
+        for (short s = 1; s < i; s++) {
+            cout << ' ';
+        }
+
+        for (short j = 1; j <= x - i + 1; j++) {
+            cout << '*';
+        }
+
+        cout << endl;
+
+    }
+        
+    return 0;
+}
