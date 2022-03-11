4d0caaa9c9f184924445a76389a7f83c14431daa
khalid bassam seyam <kbseyam@gmail.com>
2022-03-11 23:18:03 +0300

    Create

---
+++ c++/Drawing/02/_4.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    short lines;
+    do
+    {
+        cout << "Lines: ";
+        cin >> lines;
+    } while (lines < 1 || lines % 2 == 0);
+    
+    for (short i = 1; i <= lines; i++) {
+
+        for (short j = 1; j <= i; j++) {
+            cout << '*';
+        }
+
+        cout << endl;
+
+    }
+
+    for (short i = 1; i < lines; i++) {
+
+        for (short j = 1; j <= lines - i; j++) {
+            cout << '*';
+        }
+
+        cout << endl;
+
+    }
+
+    return 0;
+}
