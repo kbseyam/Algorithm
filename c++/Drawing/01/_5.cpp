58fe462ad6092cef28005247d2a6898795376340
khalid bassam seyam <kbseyam@gmail.com>
2022-03-11 22:55:23 +0300

    Create

---
+++ c++/Drawing/01/_5.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    short lines;
+    short columns;
+
+    do
+    {
+        cout << "Lines: ";
+        cin >> lines;
+        cout << "Columns: ";
+        cin >> columns;
+    } while (lines < 1 || columns < 1);
+
+    for (short l = 1; l <= lines; l++) {
+
+        for (short s = 1; s <= lines - l; s++) {
+            cout << ' ';
+        }
+
+        for (short c = 1; c <= columns; c++) {
+            cout << '*';
+        }
+
+        cout << endl;
+
+    }    
+
+    return 0;
+}
