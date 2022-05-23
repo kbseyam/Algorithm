bf77653188a9e3db8fadcdff55981d32ec42b74f
Khalid Seyam <kbseyam@gmail.com>
2022-05-23 05:52:22 +0300

    use function

--- c++/Area/Circle_Area_Inscribed_in_an_Isosceles_Triangle.cpp
+++ c++/Area/Circle_Area_Inscribed_in_an_Isosceles_Triangle.cpp
@@ -3,7 +3,13 @@ using namespace std;
 
 const float PI = 3.1415;
 
-int main() {
+float circleAreaInscribedInAnIsoscelesTriangle(float a, float b)
+{
+    return PI * ((b * b) / 4) * ((2 * a - b) / (2 * a + b));
+}
+
+int main()
+{
 
     float a;
     cout << "a: ";
@@ -12,9 +18,7 @@ int main() {
     cout << "b: ";
     cin >> b;
 
-    float area = PI * ((b * b) / 4) * ((2 * a - b) / (2 * a + b));
-
-    cout << "Area: " << area;
+    cout << "Area: " << circleAreaInscribedInAnIsoscelesTriangle(a, b);
 
     return 0;
 }
