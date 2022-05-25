8c4eefb72e33bd881ec4b0f842bec7b39bf44a53
Khalid Seyam <kbseyam@gmail.com>
2022-05-25 04:53:46 +0300

    _

--- c++/avg_grade_using_array.cpp
+++ c++/avg_grade_using_array.cpp
@@ -1,21 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-
-    float grades[3];
-
+void readGrades(float grades[3]) {  // arrays in parameter by default is ref
     cout << "Please neter grade1?\n";
     cin >> grades[0];
     cout << "Please neter grade2?\n";
     cin >> grades[1];
     cout << "Please neter grade3?\n";
     cin >> grades[2];
+}
 
-    float avg = (grades[0] + grades[1] + grades[2]) / 3;
+float calcAVGGrades(float grades[3]) {
+    return (grades[0] + grades[1] + grades[2]) / 3;
+}
+
+int main() {
+
+    float grades[3];
+
+    readGrades(grades);
 
     cout << "\n*******************************************\n";
-    cout << "The avreage of grades is " << avg;
+    cout << "The avreage of grades is " << calcAVGGrades(grades);
 
     return 0;
 }
