f5578c63e9179d48fb575c9dcac0f3de70d010aa
Khalid Seyam <kbseyam@gmail.com>
2022-07-20 20:25:56 +0300

    add

---
+++ c++/ForLoopAndArrays.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+void readArrayData(int arr[100], int &length);
+void printArrayData(int arr[100], int length);
+int calculateSumArray(int arr[100], int length);
+float calculateAvgArray(int arr[100], int length);
+
+int main() {
+
+    int arr[100], length = 0;
+
+    readArrayData(arr, length);
+    printArrayData(arr, length);
+    
+    cout << "***************************************" << endl;
+    cout << "Sum = " << calculateSumArray(arr, length) << endl;
+    cout << "Average = " << calculateAvgArray(arr, length) << endl;
+
+    return 0;
+}
+
+void readArrayData(int arr[100], int &length) {
+
+    cout << "How many numbers do you want to enter? (1 to 100)\n";
+    cin >> length;
+
+    for (int i = 0; i < length; i++) {
+        cout << "Please enter number " << i + 1 << "\n";
+        cin >> arr[i];
+    }
+}
+
+void printArrayData(int arr[100], int length) {
+    for (int i = 0; i < length; i++) {
+        cout << "Number [" << i + 1 << "] : " << arr[i] << "\n";
+    }
+}
+
+int calculateSumArray(int arr[100], int length) {
+    int sum = 0;
+
+    for (int i = 0; i < length; i++) {
+        sum += arr[i];
+    }
+
+    return sum;
+}
+
+float calculateAvgArray(int arr[100], int length) {
+    return (float)calculateSumArray(arr, length) / length;
+}
