ef8d20348cbf50bf9aaaecb1484d6d9b44ac7423
Khalid Seyam <kbseyam@gmail.com>
2022-07-29 06:26:11 +0300

    Optmize

--- c++/Sort/SelectionSort.cpp
+++ c++/Sort/SelectionSort.cpp
@@ -4,12 +4,10 @@ using namespace std;
 void selectionSort(int arr[10]);
 
 int main() {
-
-    int arr[10] = { 88, 105, 21, 60, 999, 31, 1, 44, 10, 7 };
+    int arr[10] = {88, 105, 21, 60, 999, 31, 1, 44, 10, 7};
     selectionSort(arr);
 
-    for (int i = 0; i < 10; i++)
-    {
+    for (int i = 0; i < 10; i++) {
         cout << arr[i] << '\n';
     }
 
@@ -20,21 +18,11 @@ void selectionSort(int arr[10]) {
     for (int i = 0; i < 10; i++) {
         int min = i;
         for (int j = i + 1; j < 10; j++) {
-            if (arr[j] < arr[min])
-            {
+            if (arr[j] < arr[min]) {
                 min = j;
             }
-
         }
 
         swap(arr[i], arr[min]);
-        
     }
-
-}
-
-void swap(int& first, int& second) {
-    int temp = first;
-    first = second;
-    second = temp;
 }
